f41878261788d32b0102950e3377a7a43b1619ca
Thunggg <thuannguyen20041028@gmail.com>
2025-10-24 21:26:19 +0700

    feature: build login page

---
+++ src/app/(auth)/login/login-form.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import envConfig from "@/config/config";
+import { LoginBody, LoginBodyType } from "@/schemaValidations/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+const LoginForm = () => {
+  // 1. Define your form.
+  const form = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: LoginBodyType) {
+    try {
+      const result = await fetch(
+        `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/login`,
+        {
+          body: JSON.stringify(values),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        }
+      ).then(async (res) => {
+        const payload = await res.json();
+        const data = {
+          status: res.status,
+          payload,
+        };
+        if (!res.ok) {
+          throw data;
+        }
+        return data;
+      });
+
+      toast.success("Đăng nhập thành công", {
+        position: "top-right",
+        richColors: true,
+      });
+    } catch (error: any) {
+      const errors = (error as any).payload.errors as {
+        field: string;
+        message: string;
+      }[];
+      const status = error.status as number;
+      if (status === 422) {
+        errors.forEach((error) => {
+          form.setError(error.field as "email" | "password", {
+            type: "server",
+            message: error.message,
+          });
+        });
+      } else {
+        toast.warning("Lỗi", {
+          description: error.payload.message,
+          position: "top-right",
+          richColors: true,
+        });
+      }
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-5 max-w-[600px] flex-shrink-0 w-full"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>mật khẩu</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="mt-2 w-full" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,1 +1,14 @@
-export const LoginPage = () => {};
+import LoginForm from "./login-form";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-xl font-semibold">Đăng nhập</h1>
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
